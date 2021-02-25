599858e001e85782b325103d656d4133d08189f8
Simon Lee <simonlee011040@gmail.com>
2021-02-25 17:51:20 +0000

    winner of votes identifier
    Added code to be able to identify which Party object has the most votes out of all the other party objects which are in a list.

--- Assessment_1/Assessment_1/Program.cs
+++ Assessment_1/Assessment_1/Program.cs
@@ -2,7 +2,13 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Resources;
+using System.Linq;
 //COMMENT TO TEST COLABORATIVE WORK
+
+/*what need to be done now is divide the votes,
+ * add a method to the class to assign seats with appriopriate names
+ * make the code more efficeint as it uses too many for loops.
+ */
 namespace Assessment_1
 {
     class Program
@@ -12,22 +18,58 @@ namespace Assessment_1
             List<Party> parties = new List<Party>();
             string contents = Assessment_1.Properties.Resources.Assessment1Data;
             string[] line = contents.Split("\n");
+            List<float> votesList = new List<float>();
             int seatsAvailable = int.Parse(line[1]);
             foreach (string element in line)
             {
                 if (char.IsLetter(element[0]) == true)
                 {
-                    Console.WriteLine("hit");
                     List<string> emptyList = new List<string> {};
                     string number = element.Split(",")[1];
                     Party placeHolder = new Party(element.Split(",")[0], int.Parse(number), emptyList);
                     parties.Add(placeHolder);
                 }
             }
-            foreach (Party element in parties)
+            for (int i = 0; i < seatsAvailable; i++)
             {
-                element.status();
+                foreach (Party element in parties)
+                {
+                    votesList.Add(element.votes);
+                }
+                float maxValue = votesList.Max();
+                int counter = 0;
+                int indexOfWinner = 5;
+                foreach (float element in votesList)
+                {
+                    if(element == maxValue)
+                    {
+                        indexOfWinner = counter;
+                        break;
+                    }
+                    else
+                    {
+                        counter++;
+                    }
+                }
+                int counter2 = 0;
+                foreach (Party element in parties)
+                {
+                    if(counter2 == indexOfWinner)
+                    {
+                        Console.WriteLine($"the winner is {element.name} with {element.votes} votes");
+                        break;
+                    }
+                    else
+                    {
+                        counter2++;
+                    }
+                }
+                foreach (Party element in parties)
+                {
+                    element.votesDivision();
+                }
             }
+
         }
     }
 }
