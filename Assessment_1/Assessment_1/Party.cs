28706a1daa53e92b5d0764bb86a5a163ea26e785
patpot <patpotvotr@gmail.com>
2021-03-04 10:23:51 +0000

    Code optimizations

--- Assessment_1/Assessment_1/Party.cs
+++ Assessment_1/Assessment_1/Party.cs
@@ -11,80 +11,29 @@ namespace Assessment_1
     class Party
     {
         //attributes of the object
-        private string _name;       //name of the party
-        private double _votes;      //number of votes the party has, this number changes if the party wins the round
-        private List<string> _seats;        //the list of seats that the party has won
-        private List<string> _possibleSeatNames;        //the possible names that the seats could have eg bp1,bp2,bp3,bp4,bp5
-        private double _originalVotes;      //the original number of votes that the party was givin in the input file. does not change.
-        //getters and setters for the encapsulated(private attributes)
-        public string name
-        {
-            get {return _name;}
-            set { _name = value; }
-        }
-        public double votes
-        {
-            get { return _votes; }
-            set { _votes = value; }
-        }
-        public List<string> seats
-        {
-            get { return _seats; }
-            set { _seats = value; }
-        }
-        public List<string> possibleSeatNames
-        {
-            get { return _possibleSeatNames; }
-            set { _possibleSeatNames = value; }
-        }
-        public double originalVotes
-        {
-            get { return _originalVotes; }
-            set { _originalVotes = value; }
-        }
+        public string name;       //name of the party
+        public double votes;      //number of votes the party has, this number changes if the party wins the round
+        public List<string> seats = new List<string>();        //the list of seats that the party has won
+        public List<string> possibleSeatNames = new List<string>();        //the possible names that the seats could have eg bp1,bp2,bp3,bp4,bp5
+        public double originalVotes;      //the original number of votes that the party was givin in the input file. does not change.
 
         // a constructor to initialize a new instance of the class
-        public Party(string new_name, double new_votes, List<string> new_seats)
+        public Party(string new_name, double new_votes)
         {
-
             name = new_name;
             votes = new_votes;
-            seats = new_seats;
             originalVotes = new_votes;  //this is the orginal votes which should not be changed at all. do not know how to make this into a constant.
-            possibleSeatNames = new List<string> { };
+            seats = new List<string>();
+            possibleSeatNames = new List<string>();
         }
 
-        // a method which just writes to the console who has won the round (testing purposes)
-        public void status()
-        {
-            Console.WriteLine($"{_name}\n{_votes}\n{String.Join(",",_seats)}\n\n");
-        }
-
-        //method to divide the votes. the _votes attribute is set by the originalvotes / number of seats. this means no matter how many times this is run, as long as the seat count is the same then the votes will be as well
-        public void votesDivision()
-        {
-            //this divides each of the parties votes by the number of seats they have +1 each round
-            votes = Math.Round(originalVotes / (seats.Count +1));
-        }
+        //method to divide the votes. the _votes attribute is set by the originalvotes / number of seats. 
+        //this means no matter how many times this is run, as long as the seat count is the same then the votes will be as well
+        //this divides each of the parties votes by the number of seats they have +1 each round
+        public void votesDivision() => votes = Math.Round(originalVotes / (seats.Count + 1));
 
         // adds 'seat<number>' to the seats list. no proper name just yet.
-        public void addSeats()
-        {
-            try
-            {
-                seats.Add(possibleSeatNames[seats.Count]);
-            }
-            catch (Exception)
-            {
-
-                Console.WriteLine("out of range, could not add");
-            }
-            
-        }
-        public int getSeatCount()
-        {
-            return this._seats.Count;
-        }
+        public void addSeats() => seats.Add(possibleSeatNames[seats.Count]);
         
     }
 }
--- Assessment_1/Assessment_1/Program.cs
+++ Assessment_1/Assessment_1/Program.cs
@@ -15,119 +15,68 @@ namespace Assessment_1
         static void Main(string[] args)
         {
             List<Party> parties = new List<Party>();    // creates a list with the data type of our custom object 'Party'
-            string contents = Assessment_1.Properties.Resources.Assessment1Data; // getting file data into a variable
-            string[] line = contents.Split("\n"); // splitting each line of the file data
-            int seatsAvailable = int.Parse(line[1]); // getting seats to be won (rounds) from the file data
-            foreach (string element in line)
+            string contents = Properties.Resources.Assessment1Data; // getting file data into a variable
+            string[] lines = contents.Split("\n"); // splitting each line of the file data
+            int seatsAvailable = int.Parse(lines[1]); // getting seats to be won (rounds) from the file data
+            foreach (string line in lines)
             {
-                if (char.IsLetter(element[0]) == true)
+                if (char.IsLetter(line[0]) == true)
                 {
-                    List<string> emptyList = new List<string> {};
-                    string number = element.Split(",")[1];
-                    Party placeHolder = new Party(element.Split(",")[0], int.Parse(number), emptyList);     //creats a new instance of the class (an object) each time the loop is run, setting specific values for each party
-                    parties.Add(placeHolder);
+                    string name = line.Split(",")[0];
+                    int votes = int.Parse(line.Split(",")[1]);
+                    Party party = new Party(new_name: name, new_votes: votes);     //creats a new instance of the class (an object) each time the loop is run, setting specific values for each party
+                    parties.Add(party);
+
+                    // Party has now been instantiated, lets add seat names to it
+                    List<string> seatNames = line.Split(",").ToList();
+                    seatNames.RemoveRange(0, 2); // The first two values aren't seat names, so we remove those, seatNames is now a list of all seat names for this line
+                    parties[parties.Count - 1].possibleSeatNames = seatNames; // We're only ever operating on the final value as that's the one we just added, so access the array at count - 1
                 }
             }
 
-            //adds the possible names to the party objects associate attribute. this allows the correct assigning of seat names.
-            int counter3 = 0;
-            foreach (string element in line)
-            {
-                if (char.IsLetter(element[0]) == true)
-                {
-                    string[] alist = element.Split(",");
-                    List<string> templist = new List<string>(alist);
-                    foreach (string item in templist.GetRange(2,templist.Count-2))
-                    {
-                        parties[counter3].possibleSeatNames.Add(item);
-                    }
-                    //Console.WriteLine(parties[counter3].possibleSeatNames.Count);
-                    counter3++;
-                }
-            }
-
-               //loop for each round
+            //loop for each round
             for (int i = 0; i < seatsAvailable; i++)
             {
-                List<double> votesList = new List<double>();
-                foreach (Party element in parties)
+                Dictionary<double, Party> votesList = new Dictionary<double, Party>();
+                foreach (Party party in parties)
                 {
-                    votesList.Add(element.votes);       //adds the instances votes into a list. the votes are retrieved through a public getter.
+                    votesList.Add(party.votes, party);       //adds the instances votes into a list. the votes are retrieved through a public getter.
                 }
-                double maxValue = votesList.Max();      //find which value is the largest in the list or finds what the largest votes number is
-                int counter = 0;
-                int indexOfWinner = 5;
-                foreach (double element in votesList)
-                {
-                    if(element == maxValue)
-                    {                                   //finding which party won the vote
-                        indexOfWinner = counter;        //loops through the list of votes and when a party's votes is the same as the maximum then an incremented counter is set as the index value of the winner from. which will be used to select the winner from the parties list
-                        break;
-                    }
-                    else
-                    {
-                        counter++;
-                    }
-                }
-                int counter2 = 0;
-                foreach (Party element in parties)
-                {
-                    if(counter2 == indexOfWinner)
-                    {
-                        Console.WriteLine($"the winner is {element.name} with {element.votes} votes");
-                        element.addSeats();     //when the index location matches with the counter in a loop that goes over the parties list; the winner is found. when this happens a method is called. This is a form of ABSTRACTION as the details of the method are hidden, all that the programmers need to know after writing the method is how to use it, the code is then abstracted. this method adds a set to one of the objects attributes
-                        break;
-                    }
-                    else
-                    {
-                        counter2++;
-                    }
-                }
+                double maxValue = votesList.Keys.Max();      //find which value is the largest in the list or finds what the largest votes number is
+                Party winner = votesList[maxValue];
+
+                Console.Write($"The winner is {winner.name} with {winner.votes} votes\n");
+                winner.addSeats();
 
                 //this does nothing for now since the seats are not assigned yet meaning that there will be 
-                foreach (Party element in parties)
-                {
-                    element.votesDivision();        //another method is called to divide the votes each round. abstraction too.
-
-                }
+                foreach (Party party in parties)
+                    party.votesDivision();        //another method is called to divide the votes each round. abstraction too.
             }
 
             //the code below will write the details of the winners into an output file.
-            try
+            string path = Directory.GetCurrentDirectory() + @"\output\";        //the directory path i want to place the output text in. the current directory the exe is running from + one directory deeper.
+            if (!Directory.Exists(path))         //checks if the fodler is created/there or not
             {
-                string path = Directory.GetCurrentDirectory() + @"\output\";        //the directory path i want to place the output text in. the current directory the exe is running from + one directory deeper.
-                if (File.Exists(path) == false)         //checks if the fodler is created/there or not
-                {
-                    Directory.CreateDirectory(path);
-                    StreamWriter sw = File.CreateText(path + @"output.txt");        //creates both additional directory and txt file.
-                    sw.Close();
-                }
-                using (StreamWriter sw = new StreamWriter(path + @"output.txt"))        //code below is the logic and action of writing to the file by opening it up
-                {
-                    sw.Write(line[0]);      
-                    foreach(Party element in parties)       //adds the title then loops over the parties
-                    {
-                        if (element.getSeatCount() > 0)     //only writes parties that have at least one win/seat
-                        {
-                            string entry = element.name + ",";
-                            foreach (string item in element.seats)      //writes the party's name then loops over the party's seats to be added to a string.
-                            {
-                                entry += item;
-                                if (item == element.seats[element.seats.Count - 1])
-                                    entry += ";";       //last case scenario where its the last seat and will add a semi colon.
-                                else
-                                    entry += ",";
-                            }
-                            sw.WriteLine(entry);        //writes the entry to the file.
-                        }
-                    }
-                    sw.Close();     //closes file after use.
-                }
+                Directory.CreateDirectory(path);   //creates both additional directory and txt file.
+                File.CreateText(path + @"output.txt");        //creates both additional directory and txt file.
             }
-            catch (Exception) //error handing so the program still runs in case of an error
+            using (StreamWriter sw = new StreamWriter(path + @"output.txt"))
             {
-
-                Console.WriteLine("ERROR!");
+                sw.WriteLine(lines[0]); //The title read in from the vote data
+                foreach (Party party in parties)
+                {
+                    if (party.seats.Count() > 0)
+                    {
+                        string partyData = "";
+                        partyData += party.name;
+                        foreach (string seat in party.seats)
+                        {
+                            partyData += $", {seat}";
+                        }
+                        partyData += ";";
+                        sw.WriteLine(partyData);
+                    }
+                }
             }
         }
     }
